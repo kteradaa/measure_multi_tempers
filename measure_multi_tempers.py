427a3a7e77d237bc46b72a9479083c908291640e
Koichi TERADA <kteradaa@gmail.com>
2019-11-12 23:55:43 +0900

    initial commit

---
+++ measure_multi_tempers.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+
+import subprocess
+import sys
+
+cmd_tempered_path = '../TEMPered/utils/tempered'
+cmd_hidquery_path = '../TEMPered/utils/hid-query'
+
+# --- Get HIDRAW devices as list
+
+cmd = "ls -1 /dev/hidraw*"
+res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+# sys.stdout.buffer.write( res.stdout )
+
+device_list = res.stdout.decode("utf8").splitlines() 
+# print( res.stdout.decode("utf8").splitlines() )
+
+devname = []
+vidpid = []
+physical = []
+
+for item in device_list:
+	#print("HIDRAW device found : "+item)
+
+	cmd = cmd_hidquery_path + " -p " + item
+	res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+	str = res.stdout.decode("utf8")	
+
+	query_list = res.stdout.decode("utf8").split(' ')
+	
+	#print( "DeviceID:ProductID = " + query_list[3].rstrip() )
+	#print( "Physical Address = " + query_list[8].rstrip() )
+
+	devname.append( item )
+	vidpid.append( query_list[3].rstrip() )
+	physical.append( query_list[8].rstrip() )
+
+#for dev, id, phys in zip( devname, vidpid, physical ):
+	#print( "dev = " + dev + " id = " + id + " phys = " + phys )
+
+# --- Read devlist file
+
+path = 'devlist'
+devlist_phys = []
+with open(path) as fp:
+	bufs = fp.readlines()
+	for str in bufs:
+		devlist_list = str.split(' ')
+		devlist_phys.append( devlist_list[8].rstrip() )
+
+#for phys in devlist_phys:
+	#print( "devlist phys = " + phys )
+
+# --- Looking out HID device name for each phys in devlist 
+
+meas_number = 1
+for phys in devlist_phys:
+	match_idx = physical.index( phys )
+
+	#print( "phys " , phys , " matched to index " , match_idx , " as " , physical[match_idx] )
+	#print( "phys " + phys + " device = " + devname[match_idx] + " vid/pid = " + vidpid[match_idx] )
+
+	if vidpid[match_idx] == '413d:2107' :
+	
+		cmd = cmd_tempered_path + " " + devname[match_idx] 
+		res = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+		#print( res.stdout.decode("utf8").rstrip() )
+	
+		meas_list = res.stdout.decode("utf8").splitlines() 
+		temperature = 'no measure'
+		for str in meas_list:
+			bufs = str.split(' ')
+			#print( bufs )
+			if bufs[2] == "temperature" :
+				temperature = bufs[3]
+
+		print( meas_number, phys + " " + temperature )
+		meas_number += 1
+
+# end of code
+
